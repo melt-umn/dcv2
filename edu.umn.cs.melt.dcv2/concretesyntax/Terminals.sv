13cd0fbc11476022fb6586162451df7bf45f368a
Nathaniel Ringo <remexre@gmail.com>
2017-01-26 17:52:39 -0600

    https://github.com/melt-umn/silver/issues/103

--- edu.umn.cs.melt.dcv2/concretesyntax/Terminals.sv
+++ edu.umn.cs.melt.dcv2/concretesyntax/Terminals.sv
@@ -6,21 +6,21 @@ lexer class KEYWORD dominates IDENTIFIER;
 lexer class LITERAL;
 lexer class OPERATOR;
 
-terminal Plus_t     '+' precedence = 1, association = left, lexer classes {OPERATOR};
-terminal Minus_t    '-' precedence = 1, association = left, lexer classes {OPERATOR};
-terminal Multiply_t '*' precedence = 2, association = left, lexer classes {OPERATOR};
-terminal Divide_t   '/' precedence = 2, association = left, lexer classes {OPERATOR};
+terminal Plus_t     '+' lexer classes {OPERATOR};
+terminal Minus_t    '-' lexer classes {OPERATOR};
+terminal Multiply_t '*' lexer classes {OPERATOR};
+terminal Divide_t   '/' lexer classes {OPERATOR};
 
 terminal LParen_t '(';
 terminal RParen_t ')';
+
 
 terminal Let_t 'let' lexer classes {KEYWORD};
 terminal Bind_t '='  lexer classes {OPERATOR};
 terminal In_t 'in'   lexer classes {KEYWORD};
 
-terminal Identifier_t /[A-Za-z][A-Za-z0-9]*/   lexer classes {IDENTIFIER};
-terminal Literal_t    /[\-]?[0-9]+/            lexer classes {LITERAL};
--- terminal Literal_t    /[\-]?[0-9]+([\.][0-9]*)?/ lexer classes {LITERAL};
+terminal Identifier_t /[A-Za-z][A-Za-z0-9]*/     lexer classes {IDENTIFIER};
+terminal Literal_t    /[\-]?[0-9]+([\.][0-9]*)?/ lexer classes {LITERAL};
 
-ignore terminal Whitespace_t   /[\t\r\n ]+/;
+ignore terminal Whitespace_t   /[\n\r\t\ ]+/;
 ignore terminal LineComment_t  /[\-][\-].*/ lexer classes {COMMENT};
