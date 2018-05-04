cae038728932fad94b9948675d73b1e988a4f8b1
Nathan Ringo <remexre@gmail.com>
2018-05-04 01:01:01 -0500

    Monto highlighting

--- edu.umn.cs.melt.dcv2/monto/Main.sv
+++ edu.umn.cs.melt.dcv2/monto/Main.sv
@@ -7,29 +7,46 @@ import silver:json;
 import silver:support:monto; 
 import silver:support:monto:negotiation; 
 import silver:support:monto:products; 
+import silver:support:monto:utils;
 
 function main
 IOVal<Integer> ::= args::[String] ioIn::IO
 {
-  local sn :: ServiceNegotiation =
-    serviceNegotiation(
-      protocolVersion(3, 0, 0),
-      softwareVersion(
-        "edu.umn.cs.melt.dcv2.monto",
-        nothing(),
-        nothing(),
-        nothing(),
-        nothing(),
-        nothing()),
-      [],
-      [ productDescriptor("errors", "dcv2")
-      ]);
-  local svc :: Service =
-    service(sn, [mkErrorProvider()]);
   local port :: Integer =
     if listLength(args) == 1 then
       toInt(head(args))
     else
       error("Usage: java -jar edu.umn.cs.melt.dcv2.monto <port>");
+
+  local version :: SoftwareVersion =
+    softwareVersion(
+      "edu.umn.cs.melt.dcv2.monto",
+      nothing(),
+      nothing(),
+      nothing(),
+      nothing(),
+      nothing());
+  local providers :: [ServiceProvider] =
+    [ mkErrorProvider()
+    , mkHighlightingProvider(colorize)
+    ];
+
+  local svc :: Service = simpleService(version, providers);
   return ioval(runService(svc, port, ioIn), 0);
+}
+
+function colorize
+Maybe<Color> ::= td::TerminalDescriptor
+{
+  local n :: String = td.terminalName;
+  return if startsWith("edu:umn:cs:melt:ableC:concretesyntax", n) then
+    just(paletteColor(0))
+  else if startsWith("edu:umn:cs:melt:exts:ableC:algebraicDataTypes", n) then
+    just(paletteColor(1))
+  else if startsWith("edu:umn:cs:melt:exts:ableC:cilk", n) then
+    just(paletteColor(2))
+  else if startsWith("edu:umn:cs:melt:exts:ableC:regex", n) then
+    just(paletteColor(3))
+  else
+    unsafeTrace(nothing(), print("unknown terminal: " ++ n ++ "\n", unsafeIO()));
 }
