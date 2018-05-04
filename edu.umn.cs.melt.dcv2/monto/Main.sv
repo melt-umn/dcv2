7177555e7f35a7d4773b0c1a315413223a4d3664
Nathan Ringo <remexre@gmail.com>
2018-05-04 01:54:23 -0500

    Updated colorize.

--- edu.umn.cs.melt.dcv2/monto/Main.sv
+++ edu.umn.cs.melt.dcv2/monto/Main.sv
@@ -39,14 +39,27 @@ function colorize
 Maybe<Color> ::= td::TerminalDescriptor
 {
   local n :: String = td.terminalName;
-  return if startsWith("edu:umn:cs:melt:ableC:concretesyntax", n) then
-    just(paletteColor(0))
-  else if startsWith("edu:umn:cs:melt:exts:ableC:algebraicDataTypes", n) then
-    just(paletteColor(1))
-  else if startsWith("edu:umn:cs:melt:exts:ableC:cilk", n) then
-    just(paletteColor(2))
-  else if startsWith("edu:umn:cs:melt:exts:ableC:regex", n) then
-    just(paletteColor(3))
-  else
-    unsafeTrace(nothing(), print("unknown terminal: " ++ n ++ "\n", unsafeIO()));
+  return case td.terminalName of
+  | "edu:umn:cs:melt:dcv2:concretesyntax:And_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Bind_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Else_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:End_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:False_t" -> just(literalColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Float_Literal_t" -> just(literalColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Identifier_t" -> just(identifierColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:If_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:In_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:LParen_t" -> just(punctuationColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:LessThan_t" -> just(punctuationColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Let_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Multiply_t" -> just(punctuationColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Not_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Or_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Plus_t" -> just(punctuationColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:RParen_t" -> just(punctuationColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Then_t" -> just(keywordColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:True_t" -> just(literalColor())
+  | "edu:umn:cs:melt:dcv2:concretesyntax:Whitespace_t" -> nothing()
+  | n -> unsafeTrace(nothing(), print("unknown terminal: " ++ n ++ "\n", unsafeIO()))
+  end;
 }
